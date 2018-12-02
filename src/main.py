a9fa7a7b97bf62350ee191c07bf40e5931388815
achanta1502 <achanta_pavankumar@hotmail.com>
2018-12-01 20:55:35 -0600

    final changes

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from math import isnan
+
 import pandas as pd
 import numpy as np
 import json
@@ -14,25 +16,25 @@ path = "C:/Users/achan/PycharmProjects/KaggleProject-MachineLearning/train1.csv"
 
 testPath = "C:/Users/achan/PycharmProjects/KaggleProject-MachineLearning/test2.csv"
 
-def load_df(csv_path=path, nrows=None):
-    JSON_COLUMNS = ['device', 'geoNetwork', 'totals', 'trafficSource']
+def load_data_to_dataframe(csv_path=path, nrows=None):
+    columns_json = ['device', 'geoNetwork', 'totals', 'trafficSource']
 
-    df = pd.read_csv(csv_path,
-                     converters={column: json.loads for column in JSON_COLUMNS},
+    dframe = pd.read_csv(csv_path,
+                     converters={column: json.loads for column in columns_json},
                      dtype={'fullVisitorId': 'str'},  # Important!!
                      nrows=nrows)
 
-    for column in JSON_COLUMNS:
-        column_as_df = json_normalize(df[column])
-        column_as_df.columns = [f"{column}.{subcolumn}" for subcolumn in column_as_df.columns]
-        df = df.drop(column, axis=1).merge(column_as_df, right_index=True, left_index=True)
+    for column in columns_json:
+        column_df = json_normalize(dframe[column])
+        column_df.columns = [f"{column}.{subcolumn}" for subcolumn in column_df.columns]
+        dframe = dframe.drop(column, axis=1).merge(column_df, right_index=True, left_index=True)
     # print(f"Loaded {os.path.basename(csv_path)}. Shape: {df.shape}")
-    return df
+    return dframe
 
-c = load_df(path)
-# print(c.columns)
+c = load_data_to_dataframe(path)
 
-# print(c.describe())
+
+print(c.describe())
 
 attributeMap = dict()
 count = 0.0
@@ -70,7 +72,7 @@ for column in constant_columns:
 
 # print(c.describe())
 
-test = load_df(testPath)
+test = load_data_to_dataframe(testPath)
 # print(test.columns)
 for column in test:
     if column not in columnList:
@@ -82,23 +84,19 @@ for column in test:
 
 for column in constant_columns:
     del test[column]
-# print(c[c['totals.transactionRevenue'].isnull()])
-# c["totals.transactionRevenue"].astype('float')
-# c["totals.transactionRevenue"].fillna(100000, inplace=True)
+
 y=c['totals.transactionRevenue'].values
 
-# y = pd.DataFrame({"transactions": c['totals.transactionRevenue']},dtype=np.int64)
-# print(y)
-# n_classes = y.totalTR.unique()
+
 
 #dividing the data for training and validation
 X_train,X_validation,y_train,y_validation=model_selection.train_test_split(c, y, test_size=0.20, random_state=0)
 result_val = pd.DataFrame({"fullVisitorId":X_validation['fullVisitorId'], 'transactionRevenue': X_validation["totals.transactionRevenue"]})
 del X_train['totals.transactionRevenue']
 del X_validation["totals.transactionRevenue"]
+rev = dict(map(reversed, attributeMap.items()))
 
 
-# print(c.columns)
 
 # plot for  geoNetwork Continent
 
@@ -189,13 +187,33 @@ plt.show()
 # #predict the model
 # y_pred = neural_net.predict(X_validation)
 # result_val['predictedRevenue'] = y_pred
+#
+# z = []
+# for i in result_val['predictedRevenue']:
+#     temp=rev.get(i)
+#     if isnan(float(temp)):
+#         z.append(0.0)
+#     else:
+#         z.append(float(temp))
+# result_val['predictedRevenue'] = z
+# del z
+# n = []
+# for i in result_val['transactionRevenue']:
+#     temp=rev.get(i)
+#     if isnan(float(temp)):
+#         n.append(0.0)
+#     else:
+#         n.append(float(temp))
+# result_val['transactionRevenue'] = n
+# del n
+#
 # total = result_val['transactionRevenue'].sum()
 # result_val['total']=result_val['transactionRevenue']
 # result_val['total'] = total
 #
 # #RME value
 # print('RMSE value')
-# print(np.sqrt(metrics.mean_squared_error(np.log(result_val['predictedRevenue']).values, np.log(result_val['transactionRevenue']) )))
+# print(np.sqrt(metrics.mean_squared_error(np.log1p(result_val['predictedRevenue']).values, np.log1p(result_val['transactionRevenue']) )))
 #
 # #classification Report
 # print('classification report')
@@ -209,6 +227,17 @@ plt.show()
 # y_test_pred = neural_net.predict(test)
 # result = pd.DataFrame({"fullVisitorId":test['fullVisitorId']})
 # result['predictedRevenue'] = y_test_pred
+#
+# z2= []
+# for i in result['predictedRevenue']:
+#     temp=rev.get(i)
+#     if isnan(float(temp)):
+#         z2.append(0.0)
+#     else:
+#         z2.append(float(temp))
+# result['predictedRevenue'] = z2
+# del z2
+#
 # result = result.groupby('fullVisitorId')['predictedRevenue'].sum().reset_index()
 # result.columns = ["fullVisitorId", "predictedLogRevenue"]
 # result["predictedLogRevenue"] = np.log1p(result["predictedLogRevenue"])
@@ -231,13 +260,33 @@ plt.show()
 # #predict the model
 # y_pred = svm.predict(X_validation)
 # result_val['predictedRevenue'] = y_pred
+
+# z = []
+# for i in result_val['predictedRevenue']:
+#     temp=rev.get(i)
+#     if isnan(float(temp)):
+#         z.append(0.0)
+#     else:
+#         z.append(float(temp))
+# result_val['predictedRevenue'] = z
+# del z
+# n = []
+# for i in result_val['transactionRevenue']:
+#     temp=rev.get(i)
+#     if isnan(float(temp)):
+#         n.append(0.0)
+#     else:
+#         n.append(float(temp))
+# result_val['transactionRevenue'] = n
+# del n
+
 # total = result_val['transactionRevenue'].sum()
 # result_val['total']=result_val['transactionRevenue']
 # result_val['total'] = total
 #
 # #RME value
 # print('RMSE value')
-# print(np.sqrt(metrics.mean_squared_error(np.log(result_val['predictedRevenue']).values, np.log(result_val['transactionRevenue']) )))
+# print(np.sqrt(metrics.mean_squared_error(np.log1p(result_val['predictedRevenue']).values, np.log1p(result_val['transactionRevenue']) )))
 #
 # #classification Report
 # print('classification report')
@@ -250,7 +299,18 @@ plt.show()
 # #predict the test and save it to the csv file
 # y_test_pred = svm.predict(test)
 # result = pd.DataFrame({"fullVisitorId":test['fullVisitorId']})
-# result['predictedRevenue'] = y_test_pred
+# # result['predictedRevenue'] = y_test_pred
+#
+# z2= []
+# for i in result['predictedRevenue']:
+#     temp=rev.get(i)
+#     if isnan(float(temp)):
+#         z2.append(0.0)
+#     else:
+#         z2.append(float(temp))
+# result['predictedRevenue'] = z2
+# del z2
+
 # result = result.groupby('fullVisitorId')['predictedRevenue'].sum().reset_index()
 # result.columns = ["fullVisitorId", "predictedLogRevenue"]
 # result["predictedLogRevenue"] = np.log1p(result["predictedLogRevenue"])
@@ -261,63 +321,43 @@ plt.show()
 # print('\n')
 
 
-# #Random Forest
-# print("Random Forest")
-# #build the neural network model and fit the model
-# random_forest=RandomForestClassifier(n_estimators=10,criterion= 'gini', max_depth= 3, max_features= 'sqrt')
-#
-# random_forest.fit(X_train,y_train)
-#
-# #predict the model
-# y_pred = random_forest.predict(X_validation)
-# result_val['predictedRevenue'] = y_pred
-# total = result_val['transactionRevenue'].sum()
-# result_val['total']=result_val['transactionRevenue']
-# result_val['total'] = total
-#
-# #RME value
-# print('RMSE value')
-# print(np.sqrt(metrics.mean_squared_error(np.log(result_val['predictedRevenue']).values, np.log(result_val['transactionRevenue']) )))
-#
-# #classification Report
-# print('classification report')
-# print(classification_report(y_validation, y_pred))
-#
-# #accuracy of the model
-# print('accuracy')
-# print(metrics.accuracy_score(y_validation, y_pred))
-#
-# #predict the test and save it to the csv file
-# y_test_pred = random_forest.predict(test)
-# result = pd.DataFrame({"fullVisitorId":test['fullVisitorId']})
-# result['predictedRevenue'] = y_test_pred
-# result = result.groupby('fullVisitorId')['predictedRevenue'].sum().reset_index()
-# result.columns = ["fullVisitorId", "predictedLogRevenue"]
-# result["predictedLogRevenue"] = np.log1p(result["predictedLogRevenue"])
-# min = result["predictedLogRevenue"].min()
-# result["predictedLogRevenue"] -= min
-# result.to_csv('output1.csv', index=True)
-# print(result.head(6))
-# print('\n')
-
-
-#Adaboosting
-print("Adaboosting")
+#Random Forest
+print("Random Forest")
 #build the neural network model and fit the model
-adaboost=AdaBoostClassifier(n_estimators = 100, learning_rate= 0.5, algorithm='SAMME.R' ,random_state=1)
+random_forest=RandomForestClassifier(n_estimators=10,criterion= 'gini', max_depth= 3, max_features= 'sqrt')
 
-adaboost.fit(X_train,y_train)
+random_forest.fit(X_train,y_train)
 
 #predict the model
-y_pred = adaboost.predict(X_validation)
+y_pred = random_forest.predict(X_validation)
 result_val['predictedRevenue'] = y_pred
+
+z = []
+for i in result_val['predictedRevenue']:
+    temp=rev.get(i)
+    if isnan(float(temp)):
+        z.append(0.0)
+    else:
+        z.append(float(temp))
+result_val['predictedRevenue'] = z
+del z
+n = []
+for i in result_val['transactionRevenue']:
+    temp=rev.get(i)
+    if isnan(float(temp)):
+        n.append(0.0)
+    else:
+        n.append(float(temp))
+result_val['transactionRevenue'] = n
+del n
+
 total = result_val['transactionRevenue'].sum()
 result_val['total']=result_val['transactionRevenue']
 result_val['total'] = total
 
 #RME value
 print('RMSE value')
-print(np.sqrt(metrics.mean_squared_error(np.log(result_val['predictedRevenue']).values, np.log(result_val['transactionRevenue']) )))
+print(np.sqrt(metrics.mean_squared_error(np.log1p(result_val['predictedRevenue']).values, np.log1p(result_val['transactionRevenue']) )))
 
 #classification Report
 print('classification report')
@@ -328,9 +368,20 @@ print('accuracy')
 print(metrics.accuracy_score(y_validation, y_pred))
 
 #predict the test and save it to the csv file
-y_test_pred = adaboost.predict(test)
+y_test_pred = random_forest.predict(test)
 result = pd.DataFrame({"fullVisitorId":test['fullVisitorId']})
 result['predictedRevenue'] = y_test_pred
+
+z2= []
+for i in result['predictedRevenue']:
+    temp=rev.get(i)
+    if isnan(float(temp)):
+        z2.append(0.0)
+    else:
+        z2.append(float(temp))
+result['predictedRevenue'] = z2
+del z2
+
 result = result.groupby('fullVisitorId')['predictedRevenue'].sum().reset_index()
 result.columns = ["fullVisitorId", "predictedLogRevenue"]
 result["predictedLogRevenue"] = np.log1p(result["predictedLogRevenue"])
@@ -341,6 +392,77 @@ print(result.head(6))
 print('\n')
 
 
+# #Adaboosting
+# print("Adaboosting")
+# #build the neural network model and fit the model
+# adaboost=AdaBoostClassifier(n_estimators = 100, learning_rate= 0.5, algorithm='SAMME.R' ,random_state=1)
+#
+# adaboost.fit(X_train,y_train)
+#
+# #predict the model
+# y_pred = adaboost.predict(X_validation)
+# result_val['predictedRevenue'] = y_pred
+
+# z = []
+# for i in result_val['predictedRevenue']:
+#     temp=rev.get(i)
+#     if isnan(float(temp)):
+#         z.append(0.0)
+#     else:
+#         z.append(float(temp))
+# result_val['predictedRevenue'] = z
+# del z
+# n = []
+# for i in result_val['transactionRevenue']:
+#     temp=rev.get(i)
+#     if isnan(float(temp)):
+#         n.append(0.0)
+#     else:
+#         n.append(float(temp))
+# result_val['transactionRevenue'] = n
+# del n
+
+# total = result_val['transactionRevenue'].sum()
+# result_val['total']=result_val['transactionRevenue']
+# result_val['total'] = total
+#
+# #RME value
+# print('RMSE value')
+# print(np.sqrt(metrics.mean_squared_error(np.log1p(result_val['predictedRevenue']).values, np.log1p(result_val['transactionRevenue']) )))
+#
+# #classification Report
+# print('classification report')
+# print(classification_report(y_validation, y_pred))
+#
+# #accuracy of the model
+# print('accuracy')
+# print(metrics.accuracy_score(y_validation, y_pred))
+#
+# #predict the test and save it to the csv file
+# y_test_pred = adaboost.predict(test)
+# result = pd.DataFrame({"fullVisitorId":test['fullVisitorId']})
+# result['predictedRevenue'] = y_test_pred
+
+# z2= []
+# for i in result['predictedRevenue']:
+#     temp=rev.get(i)
+#     if isnan(float(temp)):
+#         z2.append(0.0)
+#     else:
+#         z2.append(float(temp))
+# result['predictedRevenue'] = z2
+# del z2
+
+# result = result.groupby('fullVisitorId')['predictedRevenue'].sum().reset_index()
+# result.columns = ["fullVisitorId", "predictedLogRevenue"]
+# result["predictedLogRevenue"] = np.log1p(result["predictedLogRevenue"])
+# min = result["predictedLogRevenue"].min()
+# result["predictedLogRevenue"] -= min
+# result.to_csv('output1.csv', index=True)
+# print(result.head(6))
+# print('\n')
+#
+#
 # #Bagging
 #
 # print("Bagging")
@@ -351,13 +473,32 @@ print('\n')
 # #predict the model
 # y_pred = bg.predict(X_validation)
 # result_val['predictedRevenue'] = y_pred
+#
+# z = []
+# for i in result_val['predictedRevenue']:
+#     temp=rev.get(i)
+#     if isnan(float(temp)):
+#         z.append(0.0)
+#     else:
+#         z.append(float(temp))
+# result_val['predictedRevenue'] = z
+# del z
+# n = []
+# for i in result_val['transactionRevenue']:
+#     temp=rev.get(i)
+#     if isnan(float(temp)):
+#         n.append(0.0)
+#     else:
+#         n.append(float(temp))
+# result_val['transactionRevenue'] = n
+# del n
+#
 # total = result_val['transactionRevenue'].sum()
 # result_val['total']=result_val['transactionRevenue']
 # result_val['total'] = total
-#
 # #RME value
 # print('RMSE value')
-# print(np.sqrt(metrics.mean_squared_error(np.log(result_val['predictedRevenue']).values, np.log(result_val['transactionRevenue']) )))
+# print(np.sqrt(metrics.mean_squared_error(np.log1p(result_val['predictedRevenue']).values, np.log1p(result_val['transactionRevenue']) )))
 #
 # #classification Report
 # print('classification report')
@@ -371,7 +512,16 @@ print('\n')
 # y_test_pred = bg.predict(test)
 # result = pd.DataFrame({"fullVisitorId":test['fullVisitorId']})
 # result['predictedRevenue'] = y_test_pred
-# result = result.groupby('fullVisitorId')['predictedRevenue'].sum().reset_index()
+# z2= []
+# for i in result['predictedRevenue']:
+#     temp=rev.get(i)
+#     if isnan(float(temp)):
+#         z2.append(0.0)
+#     else:
+#         z2.append(float(temp))
+# result['predictedRevenue'] = z2
+# del z2
+# result = result.groupby('fullVisitorId')['predictedRevenue'].sum(). reset_index()
 # result.columns = ["fullVisitorId", "predictedLogRevenue"]
 # result["predictedLogRevenue"] = np.log1p(result["predictedLogRevenue"])
 # min = result["predictedLogRevenue"].min()
